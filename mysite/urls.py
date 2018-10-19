35e82499070f7f235d1e26611d6ca84426302412
masinde70 <masinde.masinde@metropolia.fi>
2018-10-19 12:59:04 +0300

    vurbt

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,14 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auctionsapp/', include('auctionsapp.urls')),
+    path('auctionsapp/v1/', include('auctionsapp.urls')),
+    path('auctionsapp-auth', include('rest_framework.urls')), #restframework
+   
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns = [ 
+       url(r'^__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
+
